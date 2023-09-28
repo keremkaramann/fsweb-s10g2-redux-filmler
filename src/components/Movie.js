3cb232243e6f36be4221ce70c862ed88cb3d50b6
Kerem Karaman <keremkaraman91@gmail.com>
2023-09-28 16:19:38 +0300

    ana görevler yapıldı

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
-
+import { deleteMovie } from "../actions/movieActions";
 const Movie = (props) => {
   const { id } = useParams();
   const { push } = useHistory();
@@ -9,6 +9,13 @@ const Movie = (props) => {
   const movies = useSelector((store) => store.movies);
   const movie = movies.find((movie) => movie.id === Number(id));
 
+  const dispatch = useDispatch();
+
+  const eraseMovie = (id) => {
+    const parsedId = parseInt(id, 10);
+    dispatch(deleteMovie(parsedId));
+    push("/movies");
+  };
   return (
     <div className="bg-white rounded-md shadow flex-1">
       <div className="p-5 pb-3 border-b border-zinc-200">
@@ -37,7 +44,11 @@ const Movie = (props) => {
         </div>
       </div>
       <div className="px-5 py-3 border-t border-zinc-200 flex justify-end gap-2">
-        <button type="button" className="myButton bg-red-600 hover:bg-red-500">
+        <button
+          type="button"
+          className="myButton bg-red-600 hover:bg-red-500"
+          onClick={() => eraseMovie(id)}
+        >
           Sil
         </button>
         <button className="myButton bg-blue-600 hover:bg-blue-500 ">
